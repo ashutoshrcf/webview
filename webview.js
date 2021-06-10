e39914d1064fd207bae47eea895ab042684b7753
Ashutosh Sharma <43601329+ashutoshrcf@users.noreply.github.com>
2021-06-10 17:53:46 +0530

    Update webview.js

--- webview.js
+++ webview.js
@@ -58,3 +58,15 @@ async function postData(url = '', auth, data = {}) {
 	});
 	return await response.json();
 };
+
+async function sha256(message) {
+    // encode as UTF-8
+    const msgBuffer = new TextEncoder('utf-8').encode(message);
+    // hash the message
+    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+    // convert ArrayBuffer to Array
+    const hashArray = Array.from(new Uint8Array(hashBuffer));
+    // convert bytes to hex string
+    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    return hashHex;
+    }
