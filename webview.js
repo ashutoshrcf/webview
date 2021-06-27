328be9ff1e0934e9e8f4bdbe3ab38cba314f22c5
Ashutosh Sharma <43601329+ashutoshrcf@users.noreply.github.com>
2021-06-28 00:48:57 +0530

    Update webview.js

--- webview.js
+++ webview.js
@@ -1,76 +1,93 @@
 // Post data when form is submitted
 submitForm = async function() {
-    // Get data from query string
-    const queryParams = new URLSearchParams(document.location.search);
-    const userId = queryParams.get('userId');
-    const conversationId = queryParams.get('convId');
-    const botId = queryParams.get('botId');
+  // Get data from query string
+  const queryParams = new URLSearchParams(document.location.search);
+  const userId = queryParams.get('userId');
+  const conversationId = queryParams.get('convId');
+  const botId = queryParams.get('botId');
 
-    // Get data from form
-    const vin = document.querySelector('input[name="vin"]').value;
-    const fname = document.querySelector('input[name="fname"]').value;
-    const lname = document.querySelector('input[name="lname"]').value;
-    const email = document.querySelector('input[name="email"]').value;
-    const phone = document.querySelector('input[name="phone"]').value;
-    const address = document.querySelector('input[name="addressLine1"]').value + document.querySelector('input[name="postcode"]').value;
+  // Get data from form
+  var vin = document.querySelector('input[name="vin"]').value;
+  var fname = document.querySelector('input[name="fname"]').value;
+  var lname = document.querySelector('input[name="lname"]').value;
+  var email = document.querySelector('input[name="email"]').value;
+  var contact = document.querySelector('input[name="phone"]').value;
+  var houseNo = document.querySelector('input[name="houseNumber"]').value;
+  var postcode = document.querySelector('input[name="postcode"]').value;
 
-    // use correct domain for your region
-    const domain = 'https://lo.bc-intg.liveperson.net/thirdparty-services-0.1/webview';
-    // encode auth string
-    const authString = `${conversationId} || ${botId}`;
-    const auth = await sha256(authString);
+  // process variables
+  fname = fname.trim();
+  lname = lname.trim();
+  var fullName = titleCase(fname + ' ' + lname);
 
-    const res = await postData(domain, auth, {
-        botId,
-        conversationId,
-        userId,
-        message: "SUCCESS", // optional
-        contextVariables: [{
-            "name": "vin",
-            "value": vin
-        }, {
-            "name": "fname",
-            "value": fname
-        }, {
-            "name": "lname",
-            "value": lname
-        }, {
-            "name": "email",
-            "value": email
-        }, {
-            "name": "phone",
-            "value": phone
-        }, {
-            "name": "address",
-            "value": address
-        }]
-    });
+  vin = vin.toUpperCase();
+
+  postcode = postcode.toUpperCase().replace(/\s/g, '');
+
+  function titleCase(str) {
+    str = str.toLowerCase().split(' ');
+    for (var i = 0; i < str.length; i++) {
+      str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(' ');
+  }
+
+  // use correct domain for your region
+  const domain = 'https://lo.bc-intg.liveperson.net/thirdparty-services-0.1/webview';
+  // encode auth string
+  const authString = `${conversationId} || ${botId}`;
+  const auth = await sha256(authString);
+
+  const res = await postData(auth, domain, {
+    botId,
+    conversationId,
+    userId,
+    message: "USER_FORM:SUBMITTED SUCCESSFULLY", // optional
+    contextVariables: [{
+      "name": "vin",
+      "value": vin
+    }, {
+      "name": "fullName",
+      "value": fullName
+    }, {
+      "name": "email",
+      "value": email
+    }, {
+      "name": "contact",
+      "value": contact
+    }, {
+      "name": "houseNo",
+      "value": houseNo
+    }, {
+      "name": "postcode",
+      "value": postcode
+    }]
+  });
 }
 
 
-
 // Post data to Web View API
-async function postData(url = '', auth, data = {}) {
-    const response = await fetch(url, {
-        method: 'POST',
-        mode: 'cors',
-        headers: {
-            'Authorization': auth,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    });
-    return await response.json();
-};
+async function postData(auth, url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Authorization': auth,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  return await response.json();
+}
 
 async function sha256(message) {
-    // encode as UTF-8
-    const msgBuffer = new TextEncoder('utf-8').encode(message);
-    // hash the message
-    const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-    // convert ArrayBuffer to Array
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    // convert bytes to hex string
-    const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-    return hashHex;
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder('utf-8').encode(message);
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  // convert bytes to hex string
+  const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
+  return hashHex;
 }
